224740ea48babbe84f9b78a9b67308a4e2f164f8
Urs Müller <ursmueller@gmx.ch>
2018-04-08 19:35:14 +0200

    Add Simple Particle System Example
    Add Simple Particle System Example

---
+++ SimpleParticleSystem/Simple Particle System Unit Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Simple Particle System Unit Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simple Particle System Unit Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c316c8db-847e-4f63-9a74-1905f7c4d7e8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleParticleSystem/Simple Particle System/Helper/Helper.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="Helper.cs" company="Urs Müller">
+// Copyright (c) Urs Müller. All rights reserved.
+// </copyright>
+
+namespace SimpleParticleSystem.Helper
+{
+    using System;
+
+    public class Helper
+    {
+        public const int RandMax = 32767;
+
+        private Random random;
+
+        public Helper()
+        {
+            random = new Random();
+        }
+
+        public static int RandomNumber(int iMin, int iMax)
+        {
+            if (iMin == iMax)
+            {
+                return iMin;
+            }
+
+            var random = new Random();
+            return (random.Next(RandMax) % (Math.Abs(iMax - iMin) + 1)) + iMin;
+        }
+
+        public float RandomNumber(float fMin, float fMax)
+        {
+            if (fMin == fMax)
+            {
+                return fMin;
+            }
+
+            float fRandom = (float)random.Next(RandMax) / (float)RandMax;
+            return (fRandom * Math.Abs(fMax - fMin)) + fMin;
+        }
+    }
+}
