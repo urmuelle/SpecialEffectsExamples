6cd89333bfba85cb3cf322317c9994c8a333c2f4
Urs Müller <ursmueller@gmx.ch>
2020-11-28 10:53:16 +0100

    Update on Monogame 3.8

---
+++ SimpleParticleSystem/SimpleParticleSystem_MG38/SimpleParticleSystem_MG38/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleParticleSystem_MG38
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
