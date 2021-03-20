1160ef2a1ce9e2e2554266871dbb3e825d8dda2c
Urs Müller <ursmueller@gmx.ch>
2021-03-20 19:42:54 +0100

    Upgrade Advanced Particle System to MG 3.8 Upgrade Unittests of Simple Particle System to Dotnetcore.

---
+++ AdvancedParticleSystem/AdvancedParticleSystem2019/AdvancedParticleSystem2019/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdvancedParticleSystem2019
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
