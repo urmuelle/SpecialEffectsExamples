b08cfc6d1031d2beda8b92406d05f107ccdcf2a9
Urs Müller <ursmueller@gmx.ch>
2021-03-20 20:30:03 +0100

    Upgrade Simple Explosion Example

---
+++ SimpleExplosion/SimpleExplosionMG38/Program.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="Program.cs" company="Urs Müller">
+// </copyright>
+
+namespace SimpleExplosionMG38
+{
+    using System;
+
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            using (var game = new SimpleExplosion())
+            {
+                game.Run();
+            }
+        }
+    }
+}
